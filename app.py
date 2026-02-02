95a05a03bf1c1d9818a7e1e5d8f9f1ad63c99f8b
Abhishek yadav prasad Tor <95096351+AbhishekTor55@users.noreply.github.com>
2026-02-02 14:37:30 +0530

    Change default port from 80 to 10000

--- app.py
+++ app.py
@@ -134,5 +134,5 @@ def download():
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
